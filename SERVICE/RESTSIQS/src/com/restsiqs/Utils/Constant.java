340a7b1ec8097f7a1203f981a4579e317816d909
devoutyquan <devoutyquan@163.com>
2015-10-15 10:15:36 +0800

    modify Constant

--- SERVICE/RESTSIQS/src/com/restsiqs/Utils/Constant.java
+++ SERVICE/RESTSIQS/src/com/restsiqs/Utils/Constant.java
@@ -1,9 +1,15 @@
 package com.restsiqs.Utils;
-/**
- * 常量
+
+/*
+ * 常量 
+ * Created by devouty on 2015/10/13.
  */
 
-public class Constant { 
-	
-	
+public class Constant {
+	public static final String TABLE_NAME_ACADEMY = "Academy";
+	public static final String TABLE_NAME_COURSE = "Course";
+	public static final String TABLE_NAME_NOTICE = "Notice";
+	public static final String TABLE_NAME_STUDENT = "Student";
+	public static final String TABLE_NAME_TEACHER = "Teacher";
+	public static final String TABLE_NAME_TECHNOLOGICALEXAM = "Technologicalexam";
 }

b1005c9b0344d6571ef82b02b524fc8125af640e
devoutyquan <devoutyquan@163.com>
2015-10-18 00:19:10 +0800

    Update client for testing annotations

---
+++ CLIENT/RESTSIQS/src/com/restsiqs/RESTSIQS/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.restsiqs.RESTSIQS;
+
+import android.app.Activity;
+import android.os.Bundle;
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Created by devouty on 2015/10/17.
+ */
+@EActivity(R.layout.layout_second)
+public class SecondActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.layout_second);
+    }
+}

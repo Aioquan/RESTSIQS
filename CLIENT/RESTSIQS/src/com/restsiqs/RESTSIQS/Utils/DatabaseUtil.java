72a9f5c45a74c4f9a8f1fb01cf4396455204cfec
devoutyquan <devoutyquan@163.com>
2015-10-20 23:25:53 +0800

    connect to ui & build a database util

--- CLIENT/RESTSIQS/src/com/restsiqs/RESTSIQS/Utils/DatabaseUtil.java
+++ CLIENT/RESTSIQS/src/com/restsiqs/RESTSIQS/Utils/DatabaseUtil.java
@@ -1,7 +1,29 @@
 package com.restsiqs.RESTSIQS.Utils;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by devouty on 2015/10/20.
  */
-public class DatabaseUtil {
+public class DatabaseUtil extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "restsiqs.db"; //Êý¾Ý¿âÃû³Æ
+    private static final int version = 1; //database version
+
+    public DatabaseUtil(Context context) {
+        super(context, DB_NAME, null, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "create table if not exists account(account varchar(255) not null , password varchar(255) not null ,flag int not null );";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+
 }
